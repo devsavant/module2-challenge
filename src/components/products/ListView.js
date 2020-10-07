9265e27509499ae60846c87b20101ec2bd054eb8
Julian <jarangolp@gmail.com>
2020-10-06 22:03:20 -0500

    Moved styles to an external file

--- src/components/products/ListView.js
+++ src/components/products/ListView.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import noImage from '../../no-image.png'
+import styles from './products.module.css'
 
 export default function ListView() {
     const products = useSelector(state=> state.app.products);
@@ -18,13 +19,13 @@ export default function ListView() {
     return (
         <div style={{display:"flex", justifyContent:"space-around", flexWrap:"wrap", padding: "50px"}}>
             {products.map(p =>
-            <p className="card" style={{width: "10rem", margin:"10px"}}>
-                <img className="card-img-top" style={{maxWidth: "200px", maxHeight: "200px"}}  src={p.pics[0] || noImage} alt="..." />
+            <p className={`card ${styles.productCard}`}>
+                <img className={`card-img-top ${styles.cardImg}`} src={p.pics[0] || noImage} alt="..." />
                 <div className="card-body">
                     <Link className="card-title" to={`/${p._id}`}>{p.title || 'Sin titulo'}</Link>
-                    <p style={{color: "black"}}>${p.price}MXN</p>
+                    <p className={styles.price}>{ p.price? `$ ${p.price} MXN` : 'Precio no disponible'}</p>
                     {/*<p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>*/}
-                    <button className="btn btn-primary" style={{bottom: "5px"}} onClick={()=>{
+                    <button className="btn btn-primary" onClick={()=>{
                         addItem(p)
                     }}>
                         Add to cart
